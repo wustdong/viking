0eb015fc193122628928d4ac099b75a4e0bd88fb
wustdong <1051987765@qq.com>
2024-11-19 10:53:59 +0800

    :sparkles:(transition):使用react 的非紧急更新

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useTransition } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,13 +6,24 @@ function App() {
   const [show, setShow]  = useState(true);
 
   const [counter, setCounter] = useState(0);
+  const [inputVal, setInputVal] = useState<string>();
+  const [searchVal, setSaerchVal] = useState<number[]>([]);
 
+  const [isPending, startTransition] = useTransition(); // 获取transition 的状态
   console.log('update one time--', show, counter)
   const batchUpdate = () => {
     setShow(!show);
     setCounter(counter +1);
   }
 
+  const inputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputVal(e.target.value)
+
+    startTransition(() => {
+      const arr = Array.from({length: 10000}, (_, i) => new Date().getTime() + i);
+      setSaerchVal(arr);
+    })
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +31,14 @@ function App() {
         <p>
           <button onClick={batchUpdate}>Batch Update</button>
         </p>
+        <p>
+          <input onChange={inputChange} value={inputVal}/>
+          
+        </p>
+        {isPending && <div>⏳</div>}
+        {searchVal.map((s, index) => {
+            return <div key={index}>{s}</div>
+          })}
         <a
           className="App-link"
           href="https://reactjs.org"
